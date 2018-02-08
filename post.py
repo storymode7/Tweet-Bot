9fff1333525fd7f1f6f8aacc8353a2cb1636dd11
storymode7 <17mayanksinghal@gmail.com>
2018-02-08 11:55:09 +0530

    Beautified the tweet output

--- post.py
+++ post.py
@@ -1,5 +1,6 @@
 import click
 import twitter
+from math import floor
 from credentials import tokens
 api = twitter.Api(tokens['consumer_key'], tokens['consumer_secret'],
                   tokens['access_token_key'], tokens['access_token_secret'])
@@ -10,20 +11,28 @@ def setUpdate(urlfile):
     try:
         with open('.urlFilePosition', 'r') as position:
             pos = int(position.readline())
-        if pos == len(urlfile.read()):    # Means if pos is at end of file
-            choice = input('The specified file was finished in last iteration. Want to restart it? ')
+        filelen=len(urlfile.read()) 
+        if pos == filelen:    # Means if pos is at end of file
+            choice = input('The specified file was finished in last iteration. Want to restart it? [y/n]: ')
             if choice.lower() in ['y', 'yes']:
                 pos = 0
             else:
-                click.echo('Please specify the new file as the argument by running the command again')
+                click.echo('Please specify the new file as the argument by running the command again.\n')
+                exit()
+        elif pos > filelen:
+            choice = input('The file has been modified to one with shorter lengh. Star from beginning? [y/n]: ')
+            if choice.lower() in ['y', 'yes']:
+                pos = 0
+            else:
+                click.echo('Please specify the new file as the argument by running the command again.\n')
                 exit()
         else:
-            click.echo('Continuing from the last time')
+            click.echo('Continuing from the last time.\n')
         urlfile.seek(pos)
     except ValueError:
-            click.echo('The position file is tampered. Starting from beginning.')
+            click.echo('The position file is tampered. Starting from beginning.\n')
     except FileNotFoundError:
-            click.echo('First time usage. No existing file positions found')
+            click.echo('First time usage. No existing file positions found\n')
     update = ''
     line = urlfile.readline()
     if not pos:  # Since pos not defined, means first iteration, so skip to place with first link
@@ -37,8 +46,18 @@ def setUpdate(urlfile):
         line = urlfile.readline()
     with open('.urlFilePosition', 'w') as position:
         position.write( str(urlfile.tell()-len(line)) )  # Position in file - string we just read
-#    click.echo(update.strip())
-    api.PostUpdate(update.strip())
+    update = update.strip()
+    printUpdate(update) # For testing what would be printed
+ #   api.PostUpdate(update)
+
+def printUpdate(update):
+    message='Tweet Content'
+    seperator_variable_length=floor((79 - len(message))/2)
+    seperator_variable='-'
+    seperator_string = (seperator_variable*seperator_variable_length + message + seperator_variable*seperator_variable_length)
+    print(seperator_string)
+    print(update)
+    print(seperator_variable*79)
 
 #if __name__ == '__main__':
 #    setUpdate()
